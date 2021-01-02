e515d860c93c4abf1f05e7c0cbf9e983178be08f
kamatuszewski <s16704@pjwstk.edu.pl>
2021-01-02 09:54:20 +0100

    [feature] połączenie do bazy danych

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { ormConfig } from './config/orm.config';
 import { FlashcardsModule } from './flashcards/flashcards.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [FlashcardsModule, AuthModule, UsersModule],
+  imports: [FlashcardsModule, AuthModule, UsersModule, TypeOrmModule.forRootAsync({
+    useFactory: async () => ormConfig
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/config/orm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+export const ormConfig = {
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["dist/**/**.entity{.ts,.js}"],
+  //synchronize: false,
+} as TypeOrmModuleOptions
